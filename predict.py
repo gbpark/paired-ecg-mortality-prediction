fa9f96dfb6c9a6f73cc46c2839383a088f315d64
Gibeom Park <gbpark@users.noreply.github.com>
2024-11-27 21:49:39 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,8 +4,52 @@ from sklearn import metrics
 from model import get_compiled_model
 
 def _parse_tfr_element(element):
-    # 동일한 _parse_tfr_element 함수 재사용
-    pass
+    parse_dic = {
+        'b_signal0': tf.io.FixedLenFeature([], tf.string),
+        'b_signal1': tf.io.FixedLenFeature([], tf.string),
+        'label0': tf.io.FixedLenFeature([], tf.float32),
+        'label1': tf.io.FixedLenFeature([], tf.float32),
+        'pid': tf.io.FixedLenFeature([], tf.string),
+        'dt0': tf.io.FixedLenFeature([], tf.string),
+        'dt1': tf.io.FixedLenFeature([], tf.string),
+        'delta': tf.io.FixedLenFeature([], tf.float32),
+    }
+    example_message = tf.io.parse_single_example(element, parse_dic)
+
+    b_signal0 = example_message['b_signal0']
+    b_signal1 = example_message['b_signal1']
+    signal0 = tf.io.parse_tensor(b_signal0, out_type=tf.float32)
+    signal1 = tf.io.parse_tensor(b_signal1, out_type=tf.float32)
+
+    pid = example_message['pid']
+    dt0 = example_message['dt0']
+    dt1 = example_message['dt1']
+    delta = example_message['delta']
+        
+    label0 = example_message['label0']
+    label1 = example_message['label1']
+    
+    return signal0, signal1, label0, label1, delta, pid, dt0, dt1
+
+def prepare_sample(signal0, signal1, label0, label1, delta, pid, dt0, dt1):
+    return (signal0, signal1), delta
+    
+def prepare_rev_sample(signal0, signal1, label0, label1, delta, pid, dt0, dt1):
+    return (signal1, signal0), -delta
+    
+def get_dataset(filenames, batch_size):
+    pos_dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=tf.data.AUTOTUNE)\
+                .map(_parse_tfr_element, num_parallel_calls=tf.data.AUTOTUNE)\
+                .map(prepare_sample)
+    rev_dataset = tf.data.TFRecordDataset(filenames, num_parallel_reads=tf.data.AUTOTUNE)\
+                .map(_parse_tfr_element, num_parallel_calls=tf.data.AUTOTUNE)\
+                .map(prepare_rev_sample)
+
+    dataset = tf.data.Dataset.concatenate(pos_dataset, rev_dataset)
+    dataset = dataset.shuffle(batch_size * 10, reshuffle_each_iteration=True)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    return dataset
 
 def prepare_test_sample(signal0, signal1, label0, label1, delta, pid, dt0, dt1):
     return (signal0, signal1), delta
@@ -19,7 +63,7 @@ def get_test_dataset(filenames, batch_size):
 
 if __name__ == '__main__':
     batch_size = 512
-    test_files = sorted(glob.glob('../datasets/paired-all-p*.tfrecords'))[7:]  # Test 데이터 파일
+    test_files = sorted(glob.glob('../datasets/paired-all-p*.tfrecords'))[7:]
     test_dataset = get_test_dataset(test_files, batch_size)
     model = get_compiled_model()
     model.load_weights('model_checkpoint.h5')
